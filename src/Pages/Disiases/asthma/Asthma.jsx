0f9e95d2876203b3ddadd85007954e6af8e2537c
DavidGarzonDev <juanxavif@gmail.com>
2025-04-26 11:59:59 -0500

    Estructura de modelos en la seccion asma

--- src/Pages/Disiases/asthma/Asthma.jsx
+++ src/Pages/Disiases/asthma/Asthma.jsx
@@ -1,30 +1,49 @@
-// Asthma.jsx
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Environment, SoftShadows } from '@react-three/drei';
+import { OrbitControls } from '@react-three/drei';
 import Lungs from './models-3d-asthma/Lungs';
+import Inhaler from './models-3d-asthma/Inhaler';
+import Person from './models-3d-asthma/Person';
 import './Asthma.css';
 
 const Asthma = () => {
   return (
-    <>
-    <h1>ASMA</h1>
-    
-    <Canvas shadows camera={{ position: [0, 0, 0], fov: 50 }}>
-      <ambientLight intensity={0.5} />
-      <directionalLight
-        position={[10, 10, 5]}
-        intensity={1}
-        
-        shadow-mapSize-width={1024}
-        shadow-mapSize-height={1024}
-      />
-      <OrbitControls
-      enableZoom = {false}
-      />
+    <div className="asthma-container">
+      <h1 className="asthma-title">ASMA</h1>
       
-      <Lungs  position/>
-    </Canvas>
-    </>
+      <div className="fullscreen-canvas">
+        <Canvas camera={{ position: [0, 2, 10], fov: 50 }}>
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[10, 10, 5]} intensity={1} />
+          <OrbitControls 
+            enableZoom={true}
+            minDistance={5}
+            maxDistance={20}
+          />
+          
+          {/* Modelo de Pulmones (arriba) */}
+          <group position={[0, 1.8, 0]}>
+            <Lungs />
+          </group>
+          
+          {/* Modelo de Persona (centro) */}
+          <group position={[0, -1, 0]}>
+            <Person/>
+          </group>
+          
+          {/* Modelo de Inhalador (abajo) */}
+          <group position={[0, -2, 0]}>
+            <Inhaler />
+          </group>
+          {/* Modelo de Inhalador (abajo) */}
+          
+            
+          
+        </Canvas>
+        
+      </div>
+      
+      
+    </div>
   );
 };
 
