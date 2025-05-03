9c8e8f2367bde52cc0d6a4e445b86ead11929879
DavidGarzonDev <juanxavif@gmail.com>
2025-05-03 13:55:11 -0500

    Animacion del pulmon asma

--- src/Pages/Disiases/asthma/models-3d-asthma/Lungs.jsx
+++ src/Pages/Disiases/asthma/models-3d-asthma/Lungs.jsx
@@ -1,6 +1,8 @@
 import { useGLTF } from '@react-three/drei';
 import { useThree } from '@react-three/fiber';
 import { useEffect } from 'react';
+import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
 
 const Lungs = (props) => {
     const { nodes, materials } = useGLTF('/models-3d-asthma/lungs-model.glb');
@@ -11,8 +13,17 @@ const Lungs = (props) => {
         camera.lookAt(0, 0, 0);
     }, [camera]);
 
+    const refLoung = useRef()
+    
+    useFrame((state) => {
+        const t = state.clock.getElapsedTime()
+        const scaleValue = 20 + Math.sin(t * 3) * 0.2
+        refLoung.current?.scale.set(scaleValue, scaleValue, scaleValue)
+    })
+
+
     return (
-        <group {...props} dispose={null} scale={20} position={[0, 0, 0]}>
+        <group {...props} dispose={null} scale={20} position={[0, 0, 0]} ref={refLoung}>
             <mesh 
                 geometry={nodes.Lungs.geometry} 
                 material={materials.LungsMaterial}
