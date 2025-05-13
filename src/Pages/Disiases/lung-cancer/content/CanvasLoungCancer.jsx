059ab63bf6236af07608e6802b462dc762a7e497
David <oscardavidestradabetancourth@gmail.com>
2025-05-12 22:12:51 -0500

    añadiendo texto 3d y html en escena 3d, añadiendo particulas

--- src/Pages/Disiases/lung-cancer/content/CanvasLoungCancer.jsx
+++ src/Pages/Disiases/lung-cancer/content/CanvasLoungCancer.jsx
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import "../lungCancer.css";
 import LoungCancerModelo3D from "../modelos-3d-cancer/LoungCancerModelo3D.jsx";
 import Lights from "../lights-cancer/Lights.jsx";
 import { OrbitControls } from "@react-three/drei";
 import Recipe from "../modelos-3d-cancer/Recipe.jsx";
+import Move from "../texts/Move.jsx";
 
 
 const CanvasLoungCancer = () => {
+  const [moving, setMoving] = useState(false);
+  const promptText = "Mueve con el raton!";
   return (
-    <Canvas className="canvas-loung-cancer" camera={{ position: [0, 2, 5], fov: 50,}} shadows={true} >
+    <Canvas
+      className="canvas-loung-cancer"
+      camera={{ position: [0, 2, 5], fov: 50 }}
+      shadows={true}
+      onPointerEnter={() => setMoving(true)}
+      onPointerLeave={() => setMoving(false)}
+    >
+      <Move text={promptText} hidden={moving} />
       <Lights />
       <OrbitControls
+        onStart={() => setMoving(true)}
+        onEnd={() => setMoving(false)}
         enableZoom={true} 
         enablePan={true} 
         enableRotate={true} 
       />
       <LoungCancerModelo3D />
       <Recipe />
+
     </Canvas>
   );
 };
