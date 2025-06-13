a17440b6a0005a86fd201ac17c700579bd96a6ce
DavidGarzonDev <juanxavif@gmail.com>
2025-06-13 17:47:59 -0500

    modal de la seccion tratamiento

--- src/Pages/Disiases/asthma/components/TreatmentAsthma.jsx
+++ src/Pages/Disiases/asthma/components/TreatmentAsthma.jsx
@@ -5,11 +5,14 @@ import { Canvas } from "@react-three/fiber";
 import Inhaler from "../models-3d-asthma/Inhaler";
 import Recipe from "../models-3d-asthma/Recipe";
 import { Sky } from "@react-three/drei";
+import TextTreatment from "../texts/TextTreatment";
 const TreatmentAsthma = () => {
   return (
     <Canvas
       className="canvas-treatment-asthma"
-      camera={{ position: [0, 0, 10], fov: 60 }}
+      camera={{ position: [0, 
+        0, 10], fov: 60, near: 0.1, far: 1000 }
+    }
       shadows={true}
     >
       <Lights />
@@ -17,6 +20,7 @@ const TreatmentAsthma = () => {
       <Inhaler position={[0, -1, 0]} />
       <Recipe position= {[0,0,0]} />
       <Sky/>
+      <TextTreatment/>
       
     </Canvas>
   );
---
+++ src/Pages/Disiases/asthma/texts/TextTreatment.jsx
@@ -0,0 +1,31 @@
+import { Html } from "@react-three/drei";
+import { useState } from "react";
+import './TextTreatment.css';
+
+const TextTreatment = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const closeModal = () => setIsModalOpen(false);
+    const openModal = () => setIsModalOpen(true);
+    
+    return (
+        <Html center position={[0, -3, 0]} className="text-container">
+            <div className="text-treatment">
+                <button onClick={openModal}>Información</button>
+
+                {isModalOpen && (
+                    <div className="modal-overlay" onClick={closeModal}>
+                        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                            <button className="modal-close" onClick={closeModal}>
+                                &times;
+                            </button>
+                            <h3>Toca el modelo o presiona la tecla I para más información</h3>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </Html>
+    );
+};  
+
+export default TextTreatment;
