10d0c0efc32b5772e5bcd72601dbe60d546f70f1
CamTabUnivalle <juan.camilo.taborda@correounivalle.edu.co>
2025-06-12 20:44:36 -0500

    Agregando el segundo enviroment en mi enfermedad

--- src/Pages/Disiases/pulmonary-fibrosis/content/TreatmentFibrosis.jsx
+++ src/Pages/Disiases/pulmonary-fibrosis/content/TreatmentFibrosis.jsx
@@ -4,21 +4,28 @@ import { Canvas } from "@react-three/fiber";
 import "../pulmonaryFibrosis.css";
 import LoungTreatmentFibrosis from "../models-3d-fibrosis/PillsModel3D";
 import RecipeFibrosis from "../models-3d-fibrosis/RecipeFibrosis";
+import StagingTreatment from "../staging/StagingTreatment";
 
 const TreatmentFibrosis = () => {
   return (
     <Canvas
-    className="canvas-treatmen-fibrosis"
-    shadows
-    camera={{ position: [-10, 5, 5], fov: 30 }}
-    
-  >
-    <LightsFibrosis lightPosition={[-5,5,-5]} ambIntensity={10} dirIntensity={10} />
-    <OrbitControls enableZoom={false} enablePan={false} enableRotate={true} />
-    <LoungTreatmentFibrosis position={[0,-2,0]}/>
-    <RecipeFibrosis position={[0, 0, 0]} />
-  </Canvas>
-   );
- };
+      className="canvas-treatmen-fibrosis"
+      shadows
+      camera={{ position: [6, 5, 10], fov: 40 }}
+    >
+      <LightsFibrosis
+        lightPosition={[5, 7, 5]}
+        ambIntensity={0.2}
+        dirIntensity={0.5}
+      />
+      <OrbitControls enableZoom={false} enablePan={false} enableRotate={true} />
+
+      <LoungTreatmentFibrosis position={[0, -2, 0]} />
+      <StagingTreatment />
+
+      <RecipeFibrosis position={[0, 0, 0]} />
+    </Canvas>
+  );
+};
 
 export default TreatmentFibrosis;
---
+++ src/Pages/Disiases/pulmonary-fibrosis/staging/StagingTreatment.jsx
@@ -0,0 +1,9 @@
+import { Environment } from "@react-three/drei";
+
+const StagingTreatment = () => {
+  return (
+        <Environment preset="apartment" background />
+    );
+};
+
+export default StagingTreatment;
