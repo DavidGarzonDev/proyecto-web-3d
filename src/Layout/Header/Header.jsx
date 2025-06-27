fceeef349f798329143c0576617c47b04d592ac9
CamTabUnivalle <juan.camilo.taborda@correounivalle.edu.co>
2025-06-27 11:37:31 -0500

    No deja hacer el quiz sin estar logueado

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -13,6 +13,8 @@ const Header = () => {
   const [shrink, setShrink] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
 
+  const [showModal, setShowModal] = useState(false);
+
   useEffect(() => {
     const handleScroll = () => setShrink(window.scrollY > 50);
     window.addEventListener("scroll", handleScroll);
@@ -36,113 +38,131 @@ const Header = () => {
     navigate("/");
   };
 
+  const handleQuizClick = (e) => {
+    if (!userLooged) {
+      e.preventDefault(); // evita la navegación
+      setShowModal(true); // muestra el modal
+    }
+  };
+
   return (
-    <header
-      className={`header-container${shrink ? " shrink" : ""}${
-        menuOpen ? " menu-open" : ""
-      }`}
-    >
-      <nav
-        className={`navbar${shrink ? " shrink" : ""}${
+    <>
+      {showModal && (
+        <div className="custom-modal-overlay">
+          <div className="custom-modal">
+            <p>Debes iniciar sesión para acceder al quiz.</p>
+            <button onClick={() => setShowModal(false)}>Cerrar</button>
+          </div>
+        </div>
+      )}
+
+      <header
+        className={`header-container${shrink ? " shrink" : ""}${
           menuOpen ? " menu-open" : ""
         }`}
       >
-        <Link to="/" className="logo-link" onClick={handleLinkClick}>
-          <img src="/Logo/JACO-LOGO-BLANCO.webp" className="logo-img" />
-        </Link>
-
-        <button
-          className="nav-toggle"
-          onClick={() => setMenuOpen((open) => !open)}
-          aria-label={menuOpen ? "Cerrar menú" : "Abrir menú"}
+        <nav
+          className={`navbar${shrink ? " shrink" : ""}${
+            menuOpen ? " menu-open" : ""
+          }`}
         >
-          {menuOpen ? <FiX /> : <FiMenu />}
-        </button>
+          <Link to="/" className="logo-link" onClick={handleLinkClick}>
+            <img src="/Logo/JACO-LOGO-BLANCO.webp" className="logo-img" />
+          </Link>
 
-        <ul>
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) => (isActive ? "active" : "")}
-              end
-              onClick={handleLinkClick}
-            >
-              Inicio
-            </NavLink>
-          </li>
+          <button
+            className="nav-toggle"
+            onClick={() => setMenuOpen((open) => !open)}
+            aria-label={menuOpen ? "Cerrar menú" : "Abrir menú"}
+          >
+            {menuOpen ? <FiX /> : <FiMenu />}
+          </button>
 
-          <li className="dropdown">
-            <div className="dropdown-link">Enfermedades</div>
-            <ol className="dropdown-menu">
-              <li>
-                <NavLink
-                  to="/fibrosis-pulmonar"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                  onClick={handleLinkClick}
-                >
-                  Fibrosis Pulmonar
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/asma"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                  onClick={handleLinkClick}
-                >
-                  Asma
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/cancer-pulmonar"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                  onClick={handleLinkClick}
-                >
-                  Cáncer Pulmonar
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to="/hipertension-pulmonar"
-                  className={({ isActive }) => (isActive ? "active" : "")}
-                  onClick={handleLinkClick}
-                >
-                  Hipertensión Pulmonar
-                </NavLink>
-              </li>
-            </ol>
-          </li>
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? "active" : "")}
+                end
+                onClick={handleLinkClick}
+              >
+                Inicio
+              </NavLink>
+            </li>
 
-          <li>
-            <NavLink
-              to="/quiz"
-              className={({ isActive }) => (isActive ? "active" : "")}
-              onClick={handleLinkClick}
-            >
-              Quiz
-            </NavLink>
-          </li>
+            <li className="dropdown">
+              <div className="dropdown-link">Enfermedades</div>
+              <ol className="dropdown-menu">
+                <li>
+                  <NavLink
+                    to="/fibrosis-pulmonar"
+                    className={({ isActive }) => (isActive ? "active" : "")}
+                    onClick={handleLinkClick}
+                  >
+                    Fibrosis Pulmonar
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/asma"
+                    className={({ isActive }) => (isActive ? "active" : "")}
+                    onClick={handleLinkClick}
+                  >
+                    Asma
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/cancer-pulmonar"
+                    className={({ isActive }) => (isActive ? "active" : "")}
+                    onClick={handleLinkClick}
+                  >
+                    Cáncer Pulmonar
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/hipertension-pulmonar"
+                    className={({ isActive }) => (isActive ? "active" : "")}
+                    onClick={handleLinkClick}
+                  >
+                    Hipertensión Pulmonar
+                  </NavLink>
+                </li>
+              </ol>
+            </li>
 
-          <li className="user-auth-section">
-            {userLooged ? (
-              <>
-                <div className="user-name-button">
-                  <FiUser className="user-icon" />
-                  <span>{userLooged.displayName}</span>
-                </div>
-                <button onClick={handleLogout} className="login-button">
-                  Cerrar Sesión
+            <li>
+              <NavLink
+                to="/quiz"
+                onClick={handleQuizClick}
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Quiz
+              </NavLink>
+            </li>
+
+            <li className="user-auth-section">
+              {userLooged ? (
+                <>
+                  <div className="user-name-button">
+                    <FiUser className="user-icon" />
+                    <span>{userLooged.displayName}</span>
+                  </div>
+                  <button onClick={handleLogout} className="login-button">
+                    Cerrar Sesión
+                  </button>
+                </>
+              ) : (
+                <button onClick={handleLogin} className="login-button">
+                  Inicia Sesión
                 </button>
-              </>
-            ) : (
-              <button onClick={handleLogin} className="login-button">
-                Inicia Sesión
-              </button>
-            )}
-          </li>
-        </ul>
-      </nav>
-    </header>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 };
 
